3315cdcc140692f097837b836858d765ddb12f21
Kartik <kartikbindra_23cs211@dtu.ac.in>
2023-12-19 19:13:34 +0530

    sponsors page added

---
+++ app/Components/Sponsors/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import './style.css';
+import 'tailwindcss/tailwind.css';
+
+
+const Sponsors = () => {
+    return (
+    <>
+        <p className="xl:text-7xl lg:text-7xl md:text-6xl sm:text-5xl max-[640px]:text-5xl flex justify-center mt-32 ">
+            Sponsors
+        </p>
+
+        <p className="xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-3xl text-yellow-500 flex justify-center mt-20">
+            Gold Sponsor
+        </p>
+        <div className='flex justify-center mt-10'>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+        </div>
+
+        <p className="xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-3xl text-stale-100 flex justify-center mt-20">
+            Silver Sponsors
+        </p>
+        <div className='flex justify-center flex-wrap mt-10'>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+        </div>
+
+        <p className="xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-3xl text-orange-400 flex justify-center mt-20">
+            Bronze Sponsors
+        </p>
+        <div className='flex justify-center flex-wrap mt-10'>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+        </div>
+        <div className='flex flex-row justify-center flex-wrap mb-32'>
+        <div className='mx-20'>
+        <p className="xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-3xl text-orange-400 flex justify-center mt-20">
+            Food Partners
+        </p>
+        <div className='flex justify-center flex-wrap mt-10'>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className='mx-20'>
+        <p className="xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-3xl text-orange-400 flex justify-center mt-20">
+            Event Partners
+        </p>
+        <div className='flex justify-center flex-wrap mt-10'>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+            <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center mx-10 my-4">
+                <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center">
+                    <img className="w-40 h-14 flex" src='/logo.png' alt="logo"/>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </>
+    );
+};
+
+
+export default Sponsors;
