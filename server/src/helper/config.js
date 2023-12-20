424491da72f4c209b854b80b5ba7067dab8be789
Ayush Gupta <ag8350961@gmail.com>
2023-12-21 00:42:18 +0530

    updates

--- server/src/helper/config.js
+++ server/src/helper/config.js
@@ -2,9 +2,7 @@ const mongoose= require('mongoose')
 
 const  connectDb = async ()=>{
     try{
-        const {connection} = await mongoose.connect("mongodb://127.0.0.1:27017/invictus",{
-            dbName:"work",
-        });
+        const {connection} = await mongoose.connect("mongodb://127.0.0.1:27017/invictus_website");
         console.log("connected");
     }
     catch(err){
--- server/src/model/user.js
+++ server/src/model/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username:{
         type : String,
-     //   required:[true,"please enter the name"],
+        required:[true,"please enter the name"],
         unique: true,
     },
     email:{
@@ -10,26 +10,15 @@ const userSchema = new mongoose.Schema({
         required:[true,"please enter the email"],
         unique: true,
     },
-    team_name:{
-        type : String,
-      //  required:[true,"please enter the team_name"],
-        unique: true,
-    },
     college:{
         type : String,
-     //   required:[true,"please enter the college"],
+        required:[true,"please enter the college"],
     },
     phone:{
-        type : String,
-     //   required:[true,"please enter the name"],
+        type : Number,
+        required:[true,"please enter the name"],
         unique: true,
-    },
-    isVerified:{
-        type: Boolean,
-        default: false,
-    },
-    
-    
+    },    
 });
 
 const User = mongoose.models.User || mongoose.model("User",userSchema);
