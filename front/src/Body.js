4584c20c18fd85b10b3aa8d991797d8df2f42d53
prashant0664 <prashantthakran0664@gmail.com>
2023-12-25 18:38:32 +0530

    merged AyusGap branch

--- front/src/Body.js
+++
@@ -1,5 +0,0 @@
-export default function Body(){
-    return(
-        <>HI THIS SECRET</>
-    );
-}
--- front/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/user/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    authenticate: authReducer,
-  },
-})
