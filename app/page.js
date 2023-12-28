81d6317bbf73107933d606bbe8999a2c54d8015b
Bhavit Jain <jainbhavit2018@gmail.com>
2023-12-28 19:51:14 +0530

    registration page completed

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 import React from 'react'
 import Home from "./Home/Home"
 import { Profile } from './Profile/Profile'
+import Registration from './Components/Registration-form/Registration'
 
 
 const page = () => {
   return (
-      // <Home />
-      <Profile/>
+      <Home />
+      // <Profile/>
+      // <Registration/>
   )
 }
 
