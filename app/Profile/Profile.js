1417754d488215aab5b2d52bdb0c1731bce092b3
TeekshaHarish <102624456+TeekshaHarish@users.noreply.github.com>
2023-12-29 17:46:00 +0530

    Adding form

--- app/Profile/Profile.js
+++ app/Profile/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { TeamStatus } from './TeamStatus/TeamStatus'
+import WorkshopCard from './EventCard/WorkshopCard'
+import { Form } from './Form/Form'
 
 export const Profile = () => {
   return (
     <>
+    <Form/>
     <TeamStatus/>
+    {/* <WorkshopCard/> */}
     </>
   )
 }
