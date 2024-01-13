3ae49bfb11096fc7804ed629e2eaeac83898b529
nikhil5731 <nikhilgraphicd@gmail.com>
2024-01-14 01:05:44 +0530

    Footer Changed2 --Nikhil

--- app/Components/Footer/index.js
+++ app/Components/Footer/index.js
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer flex flex-col min-[844px]:flex-row items-center justify-center gap-0 px-10 min-[844px]:gap-24 h-[20vh]">
-      <div className="border-r-2 px-10 border-white hidden min-[844px]:inline">
+    <footer className="footer flex flex-col min-[844px]:flex-row items-center justify-center gap-0 px-10 min-[844px]:gap-20 h-[20vh]">
+      <div className=" hidden min-[844px]:inline ">
         <Link href="/">
           <img
             src="/invictuslogo2.png"
@@ -15,10 +15,12 @@ const Footer = () => {
           />
         </Link>
       </div>
-      <div className="text-white font-retrog border-b-2 pb-2 min-[844px]:border-b-0 text-[8px] min-[321px]:text-sm  min-[927px]:text-xl text-center ">
+      <div className="w-1 bg-white h-[50%] rounded-full  hidden min-[844px]:inline"></div>
+      <div className="text-white font-retrog border-b-2 pb-2  min-[844px]:border-b-0 text-[8px] min-[321px]:text-[12px]  min-[927px]:text-xl text-center ">
         @Designed and Developed by ❤️ Invictus Team
       </div>
-      <div className="flex flex-col items-center gap-1 pt-2 min-[844px]:pt-0 min-[844px]:border-l-2 px-10">
+      <div className="w-1 bg-white h-[50%] rounded-full  hidden min-[844px]:inline"></div>
+      <div className="flex flex-col items-center gap-1 pt-2  min-[844px]:pt-0 ">
         <div className="flex">
           <a
             href="https://www.instagram.com/invictus_dtu/"
