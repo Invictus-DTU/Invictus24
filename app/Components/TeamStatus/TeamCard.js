546a06aebc705296517b7a9165aefbe6158ed7b2
prashant0664 <prashantthakran0664@gmail.com>
2024-01-14 15:05:32 +0530

    Profile pic and profile cards fixed

--- app/Components/TeamStatus/TeamCard.js
+++ app/Components/TeamStatus/TeamCard.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export default function TeamCard({property}) {
+export default function TeamCard({ property }) {
   return (
-    <div className='event flex w-100'>
-            
-        <img src="/arrow.png" className='mr-5' />
-        <div className='title flex flex-col'>
-            <div>{property.eventName?.type} Name: {property.eventName?.name}</div>
-            <div>Team Name: {property.teamname}</div>
-            <div>Team Id: {property.teamId}</div>
-            <div>Team status: {property.status}</div>
+    <div className="event flex w-full mx-8 mb-10">
+      <img src="/arrow.png" className="mr-5 w-10 h-10 sm:block max-sm:hidden" />
+      <div className="flex flex-wrap">
+        <div className=" flex flex-col min-[400px]:text-3xl max-[400px]:text-2xl mr-8">
+          <div>
+            {property.eventName?.type} Name: {property.eventName?.name}
+          </div>
+          <div>Team Name: {property.teamname}</div>
+          <div>Team Id: {property.teamId}</div>
+          <div>Team status: {property.status}</div>
         </div>
-        <div className='names flex flex-col'>
-            {property.member.map((val, index)=>{
-                if(val._id===property.teamLeader._id){
-                    return (<div key = {index}>{val.username} (Leader)</div>)
-                }
-            return (<div key = {index}>{val.username} member</div>)})}
+        <div className="names flex flex-col text-3xl">
+          {property.member.map((val, index) => {
+            if (val._id === property.teamLeader._id) {
+              return <div key={index}>{val.username} (Leader)</div>;
+            }
+            return <div key={index}>{val.username} member</div>;
+          })}
         </div>
+      </div>
     </div>
-  )
+  );
 }
