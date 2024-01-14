fda1abc9cfeccd726ea3cea228286e24838ed1a5
Ayush Gupta <ag8350961@gmail.com>
2024-01-14 14:12:01 +0530

    updates

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,6 @@ const userSchema = new mongoose.Schema({
     username:{
         type : String,
         required:[true,"please enter the name"],
-        unique: true,
     },
     email:{
         type : String,
