e6bb8147912a5b6896fa7003aeccc83f42718b3a
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2024-01-14 14:50:46 +0530

    profile

--- app/Components/TeamStatus/TeamStatus.js
+++ app/Components/TeamStatus/TeamStatus.js
@@ -1,21 +1,19 @@
-'use client'
-import React,{useState,useEffect} from 'react'
-import "./TeamStatus.css"
+"use client";
+import React, { useState, useEffect } from "react";
+import "./TeamStatus.css";
 import TeamCard from "./TeamCard";
 
-export const TeamStatus = ({teams}) => {
-    const [team,setTeam] = useState(teams);
-    useEffect(()=>{
-        
-        setTeam(teams);
-        console.log("hi:",team);
-    },[teams])
+export const TeamStatus = ({ teams }) => {
+  const [team, setTeam] = useState(teams);
+  useEffect(() => {
+    setTeam(teams);
+    console.log("hi:", team);
+  }, [teams]);
   return (
-    <div className="TeamStatus">
-        <div className='heading'>Events Status:</div>
-        {team && team.map((val, index)=>(
-            <TeamCard key = {index} property={val}/>
-        ))}
+    <div className="TeamStatus md:w-4/5 max-md:w-full mx-auto my-16">
+      <div className="heading text-5xl">Events Status</div>
+      {team &&
+        team.map((val, index) => <TeamCard key={index} property={val} />)}
     </div>
-  )
-}
+  );
+};
