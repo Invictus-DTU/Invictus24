ed0f85950f305b6bf14c6df30720b04df82eb4d8
nikhil5731 <nikhilgraphicd@gmail.com>
2024-01-14 16:18:47 +0530

    Speed insight added

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 import SessionProvider from "./Components/SessionProvider";
 import Protection from "./Components/Protection";
 import { Suspense } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
@@ -19,14 +20,15 @@ export default function RootLayout({ children }) {
       />
       <link rel="icon" type="image/x-icon" href="static/favicon.ico"></link>
       <body className={inter.className}>
-        <SessionProvider>
-          <Protection />
-          <Suspense fallback={<div>Loading...</div>}>
-            <Navbar />
-          </Suspense>
-          {children}
-          <Footer />
-        </SessionProvider>
+        <SpeedInsights/>
+          <SessionProvider>
+            <Protection />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Navbar />
+            </Suspense>
+            {children}
+            <Footer />
+          </SessionProvider>
       </body>
     </html>
   );
