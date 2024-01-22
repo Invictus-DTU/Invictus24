bdb8ba60e159b220a4a097a6cd13c203a83d07c4
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-01-22 20:20:33 +0530

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -32,7 +32,7 @@ const EventForm = () => {
   const imageUpload = (e) =>{
     const { name, value } = e.target;
     const id = value.match(/\/d\/([^\/]+)\/view/);
-    const url = id?"https://lh3.googleusercontent.com/d/" + id[1] + "=s220" :"";
+    const url = id?"https://lh3.googleusercontent.com/d/" + id[1] :"";
     setFormData({
       ...formData,
       image: url
