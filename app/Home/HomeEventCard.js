55f73a87539e2dfca05a70083005127c8949744e
jainbhavit2018 <jainbhavit2018@gmail.com>
2024-01-24 00:52:18 +0530

    home trophy fixed

--- app/Home/HomeEventCard.js
+++ app/Home/HomeEventCard.js
@@ -39,28 +39,28 @@ const EventCard = (props) => {
             {props.data?.name}
           </div>
         </div>
-{/*<div className="flex items-center gap-2 max-xl:hidden">
-          <img
+        <div className="flex items-center gap-2 max-xl:hidden">
+          {/* <img
             className="h-10 max-md:h-10 max-sm:h-6 w-auto  "
             src="./Trophy.png"
             alt="Trophy"
             height="100%"
-          />
+          /> */}
           <div className="text-2xl max-md:text-xl font-retrog">
-            {props.data?.prize.toString().substring(0,2) + "K" || 0}
+            {"🏆" + props.data?.prize.toString().substring(0,2) + "K" || 0}
           </div>
         </div>
         <div className="flex items-center gap-2 xl:hidden">
-            <img
+            {/* <img
             className="h-12 max-md:h-10 max-sm:h-8"
             src="./Trophy.png"
             alt="Trophy"
             height="100%"
-          />
+          /> */}
           <div className="text-2xl max-md:text-xl font-retrog">
-            {props.data?.prize || 0}
+            {"🏆" + props.data?.prize || 0}
           </div>
-        </div>*/}
+        </div>
 
         <div className="location text-center max-md:text-sm ">
           <div className="date font-retrog text-nowrap text-center w-full">
