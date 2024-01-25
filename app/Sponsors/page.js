5d77d433e8a7390198c43e5259ce424ecb1fb7bd
Prashant <98750053+Prashant0664@users.noreply.github.com>
2024-01-25 14:29:32 +0530

    Update page.js of sponsors

--- app/Sponsors/page.js
+++ app/Sponsors/page.js
@@ -1,13 +1,22 @@
 import React from "react";
-
+import Link from "next/link";
 const Sponsors = () => {
+  
   return ( 
     <div className="w-full Sponsors pb-[60px] pt-[80px]">
       <p className="font-retrog xl:text-7xl lg:text-7xl md:text-6xl sm:text-5xl max-[640px]:text-4xl flex justify-center mt-20 text-white">
         Sponsors
       </p>
-
-      <p className="font-retrog xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-2xl text-yellow-500 flex justify-center mt-20">
+      <div className="flex justify-center mt-10">
+        <div className="w-52 h-52 flex justify-center rounded-3xl bg-black/[0.3] drop-shadow-[4px_0px_40px_rgba(255,255,255,0.33)] items-center">
+          <Link href="https://adobe.com/" target="_blank" >
+           <div className="w-48 h-48 flex justify-center rounded-3xl bg-white items-center" >
+            <img className="h-46 flex" src="/adobe.png" alt="logo" />
+          </div>
+          </Link> 
+        </div>
+      </div>
+      {/* <p className="font-retrog xl:text-4xl lg:text-4xl md:text-3xl sm:text-3xl max-[640px]:text-2xl text-yellow-500 flex justify-center mt-20">
         Gold Sponsor
       </p>
       <div className="flex justify-center mt-10">
@@ -104,7 +113,7 @@ const Sponsors = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
