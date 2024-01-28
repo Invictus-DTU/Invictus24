fbf26e43f9a7e23fe6d63b0b4d13e241635df461
Prashant <98750053+Prashant0664@users.noreply.github.com>
2024-01-28 15:18:36 +0530

    Update EventCard.js

--- app/Events/EventCard.js
+++ app/Events/EventCard.js
@@ -105,11 +105,11 @@ const EventCard = (props) => {
           </>
           ) : <></>}
 
+          </>
+          ) : <></>}
           <div className=" font-ticketing xl:text-lg sm:text-sm max-[640px]:text-base">
             {props.data?.description}
           </div>
-          </>
-          ) : <></>}
 
           <div className="w-56 text-2">
             <a
