0c17e8200a90d1abdbb884a7cdf3a4cd488d25af
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-02-03 13:39:57 +0530

    Update index.js

--- app/Components/Popup/index.js
+++ app/Components/Popup/index.js
@@ -22,7 +22,7 @@ const Popup = () => {
         const hours = Math.floor(timeDifferenceInMillis % (24 * 1000 * 60 * 60)/ (1000 * 60 * 60));
         const minutes = Math.floor((timeDifferenceInMillis % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeDifferenceInMillis % (1000 * 60)) / 1000);
-        setCountDown(`${days}:${hours}:${minutes}:${seconds}`);
+        setCountDown(`${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`);
       }
     };
 
