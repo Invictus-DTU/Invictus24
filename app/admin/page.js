c7f7757f6758b378efef977cbbbd6d78742e5d6f
Prashant <98750053+Prashant0664@users.noreply.github.com>
2024-02-05 18:35:22 +0530

    revert last commit Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -69,199 +69,201 @@ const EventForm = () => {
 
   return (
     
-    <div className="flex justify-center items-center min-h-screen bg-slate-500">
-      <Toaster position="top-center" reverseOrder={false} />
-      <form onSubmit={handleSubmit} className="max-w-lg w-full bg-white p-6 rounded-md shadow-md mt-[7rem]">
-        <label className="block mb-4">
-          Event Name:
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    // <div className="flex justify-center items-center min-h-screen bg-slate-500">
+      // <Toaster position="top-center" reverseOrder={false} />
+    
+    //   <form onSubmit={handleSubmit} className="max-w-lg w-full bg-white p-6 rounded-md shadow-md mt-[7rem]">
+    //     <label className="block mb-4">
+    //       Event Name:
+    //       <input
+    //         type="text"
+    //         name="name"
+    //         value={formData.name}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Society Name:
-          <input
-            type="text"
-            name="societyName"
-            value={formData.societyName}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Society Name:
+    //       <input
+    //         type="text"
+    //         name="societyName"
+    //         value={formData.societyName}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Google Map Location:
-          <input
-            type="text"
-            name="location"
-            value={formData.location}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Google Map Location:
+    //       <input
+    //         type="text"
+    //         name="location"
+    //         value={formData.location}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Description:
-          <input
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Description:
+    //       <input
+    //         type="text"
+    //         name="description"
+    //         value={formData.description}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Date:
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Date:
+    //       <input
+    //         type="date"
+    //         name="date"
+    //         value={formData.date}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Unstop Registeration Link:
-          <input
-            type="text"
-            name="unstop"
-            value={formData.unstop}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Unstop Registeration Link:
+    //       <input
+    //         type="text"
+    //         name="unstop"
+    //         value={formData.unstop}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Team Size MIN:
-          <input
-            type="number"
-            name="teamSizeMIN"
-            value={formData.teamSizeMIN}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Team Size MIN:
+    //       <input
+    //         type="number"
+    //         name="teamSizeMIN"
+    //         value={formData.teamSizeMIN}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-        Team Size Max:
-          <input
-            type="number"
-            name="teamSizeMax"
-            value={formData.teamSizeMax}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //     Team Size Max:
+    //       <input
+    //         type="number"
+    //         name="teamSizeMax"
+    //         value={formData.teamSizeMax}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-        Prize:
-          <input
-            type="number"
-            name="prize"
-            value={formData.prize}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //     Prize:
+    //       <input
+    //         type="number"
+    //         name="prize"
+    //         value={formData.prize}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Venue:
-          <input
-            type="text"
-            name="venue"
-            value={formData.venue}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //       Venue:
+    //       <input
+    //         type="text"
+    //         name="venue"
+    //         value={formData.venue}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-        Registration End Date:
-          <input
-            type="date"
-            name="registrationEndDate"
-            value={formData.registrationEndDate}
-            onChange={handleChange}
-            className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-          />
-        </label>
+    //     <label className="block mb-4">
+    //     Registration End Date:
+    //       <input
+    //         type="date"
+    //         name="registrationEndDate"
+    //         value={formData.registrationEndDate}
+    //         onChange={handleChange}
+    //         className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //       />
+    //     </label>
 
-        <label className="block mb-4">
-          Select Event Type:
-          <div>
-            <label htmlFor="event">
-              <input
-                type="radio"
-                id="event"
-                name="type"
-                value="Events"
-                checked={formData.type === 'Events'}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full rounded-md border border-gray-300 bg-white text-gray-900"
-                required
-              />
-              Event
-            </label>
-            <label htmlFor="workshop">
-              <input
-                type="radio"
-                id="workshop"
-                name="type"
-                value="Workshops"
-                checked={formData.type === 'Workshops'}
-                onChange={handleChange}
-                className="mt-1 p-2 w-full rounded-md border border-gray-300 bg-white text-gray-900"
-                required
-              />
-              Workshop
-            </label>
-          </div>
-        </label>
+    //     <label className="block mb-4">
+    //       Select Event Type:
+    //       <div>
+    //         <label htmlFor="event">
+    //           <input
+    //             type="radio"
+    //             id="event"
+    //             name="type"
+    //             value="Events"
+    //             checked={formData.type === 'Events'}
+    //             onChange={handleChange}
+    //             className="mt-1 p-2 w-full rounded-md border border-gray-300 bg-white text-gray-900"
+    //             required
+    //           />
+    //           Event
+    //         </label>
+    //         <label htmlFor="workshop">
+    //           <input
+    //             type="radio"
+    //             id="workshop"
+    //             name="type"
+    //             value="Workshops"
+    //             checked={formData.type === 'Workshops'}
+    //             onChange={handleChange}
+    //             className="mt-1 p-2 w-full rounded-md border border-gray-300 bg-white text-gray-900"
+    //             required
+    //           />
+    //           Workshop
+    //         </label>
+    //       </div>
+    //     </label>
 
 
-        <label htmlFor="profileImage" className="block mb-4">
-          Image
-        <input
-          type="text"
-          name="image"
-          value={formData.image}
-          onChange={imageUpload}
-          className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-        />
-        </label>
+    //     <label htmlFor="profileImage" className="block mb-4">
+    //       Image
+    //     <input
+    //       type="text"
+    //       name="image"
+    //       value={formData.image}
+    //       onChange={imageUpload}
+    //       className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //     />
+    //     </label>
 
-        <label htmlFor="profileImage" className="block mb-4">
-          Read More
-        <input
-          type="text"
-          name="readmore"
-          value={formData.readmore}
-          onChange={handleChange}
-          className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
-        />
-        </label>
+    //     <label htmlFor="profileImage" className="block mb-4">
+    //       Read More
+    //     <input
+    //       type="text"
+    //       name="readmore"
+    //       value={formData.readmore}
+    //       onChange={handleChange}
+    //       className="block w-full border-gray-300 rounded-md mt-1 focus:ring-indigo-500 focus:border-indigo-500"
+    //     />
+    //     </label>
 
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+    //     <button
+    //       type="submit"
+    //       className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
+    //     >
+    //       Submit
+    //     </button>
+    //   </form>
+    // </div>
+    
     // <>
-    // {/* <div className='w-[100%] h-auto'>
-    //   <img src="https://imgk.timesnownews.com/story/Jai_Shri_Ram_1.jpg" className='w-[100%] mt-20'/>
-    //   <img src="https://www.jaipurstuff.com/wp-content/uploads/2020/08/Ram-Temple-at-Ayodhya.jpg" className='w-[100%]'/>
-    //   <img src="https://img.jagrantv.com/article/rc1048660/1704301182-ayodhya-ram-mandir-architecture.jpg" className='w-[100%]'/>
-    // </div> */}
+     <div className='w-[100%] h-auto'>
+      <img src="https://imgk.timesnownews.com/story/Jai_Shri_Ram_1.jpg" className='w-[100%] mt-20'/>
+      <img src="https://www.jaipurstuff.com/wp-content/uploads/2020/08/Ram-Temple-at-Ayodhya.jpg" className='w-[100%]'/>
+      <img src="https://img.jagrantv.com/article/rc1048660/1704301182-ayodhya-ram-mandir-architecture.jpg" className='w-[100%]'/>
+     </div>
 
 
     // </>
